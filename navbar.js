af407bcf7e9ab2b257035ea9269a54fbefb4687e
developersamuel <samuel@mecode.asia>
2021-04-12 23:31:53 +0700

    add: gitigmoreadded

--- navbar.js
+++ navbar.js
@@ -15,17 +15,34 @@
 //   }
 // }
 
+// const mainMenu = document.querySelector(".mainMenu");
+// const closeMenu = document.querySelector(".closeMenu");
+// const openMenu = document.querySelector(".openMenu");
+
+// openMenu.addEventListener("click", show);
+// closeMenu.addEventListener("click", close);
+
+// function show() {
+//   mainMenu.style.display = "flex";
+//   mainMenu.style.top = "0";
+// }
+// function close() {
+//   mainMenu.style.top = "-100%";
+// }
+
+const getShit = document.getElementById("menuElement");
 const mainMenu = document.querySelector(".mainMenu");
-const closeMenu = document.querySelector(".closeMenu");
 const openMenu = document.querySelector(".openMenu");
 
-openMenu.addEventListener("click", show);
-closeMenu.addEventListener("click", close);
+getShit.addEventListener("click", show);
 
 function show() {
-  mainMenu.style.display = "flex";
-  mainMenu.style.top = "0";
-}
-function close() {
-  mainMenu.style.top = "-100%";
+  if (!getShit.classList.contains("is-active")) {
+    getShit.classList.add("is-active");
+    mainMenu.style.display = "flex";
+    mainMenu.style.top = "0";
+  } else {
+    getShit.classList.remove("is-active");
+    mainMenu.style.top = "-100%";
+  }
 }
